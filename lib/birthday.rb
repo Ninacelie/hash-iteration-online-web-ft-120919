422b84712a5c127f2fa71ffcc712a590d1b15703
Nina Angelo <ninacelie@gmail.com>
2019-12-12 23:26:35 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,6 +16,7 @@ end
     #"Sarah" => 6,
     #"Amanda" => 27}
 
-def age_appropriate_birthday
-  puts "Happy Birthday Timmy! You are now 9 years old! Happy Birthday Sarah! You are now 6 years old! You are too old for this."
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
 end
