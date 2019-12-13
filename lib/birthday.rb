a5a0b83f29f72ec7693b205805fc2ff6e863934a
Nina Angelo <ninacelie@gmail.com>
2019-12-13 00:46:52 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -22,6 +22,7 @@ def age_appropriate_birthday(birthday_kids)
   puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
   end
+  birthday_kids.each do |kids_name, age|
   if age <= 6
     puts "You are too old for this."
   end
